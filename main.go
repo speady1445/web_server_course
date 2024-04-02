17fcbb76ae5c26b1a7e392041365af4760f8b590
speady1445 <sykora1445@gmail.com>
2024-04-02 20:25:38 +0200

    Add chirp validation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -15,6 +16,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /api/healthz", healthz)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpValidate)
 	corsMux := middlewareCors(mux)
 
 	server := http.Server{
@@ -76,4 +78,41 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, _ *http.Request) {
 
 </html>
 	`, cfg.fileserverHits)))
+}
+
+func handlerChirpValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnError struct {
+		Error string `json:"error"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		returnVal, _ := json.Marshal(returnError{Error: "Invalid input"})
+		respondWith(w, http.StatusBadRequest, returnVal)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		returnVal, _ := json.Marshal(returnError{Error: "Chirp is too long"})
+		respondWith(w, http.StatusBadRequest, returnVal)
+		return
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	returnVal, _ := json.Marshal(returnVals{Valid: true})
+	respondWith(w, http.StatusOK, returnVal)
+}
+
+func respondWith(w http.ResponseWriter, status int, body []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(body)
 }
