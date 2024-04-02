98d1b7dc75c15c17a2fbf90238061637d894f0ff
speady1445 <sykora1445@gmail.com>
2024-04-02 08:06:48 +0200

    Add healthz status endpoint

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthz)
 	corsMux := middlewareCors(mux)
 
 	server := http.Server{
@@ -32,4 +33,10 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
